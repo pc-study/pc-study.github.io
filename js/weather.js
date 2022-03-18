d90448df8005112129c7ab22ae182ea5b4dd29fb
pc1107750981@163.com <pc1107750981@163.com>
2022-03-18 20:03:12 +0800

    Site updated: 2022-03-18 20:03:11

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,23 @@
-WIDGET={CONFIG:{modules:"01234",background:"5",tmpColor:"FFFFFF",tmpSize:"20",cityColor:"FFFFFF",citySize:"20",aqiColor:"FFFFFF",aqiSize:"20",weatherIconSize:"24",alertIconSize:"20",padding:"0px 0px 0px 0px",shadow:"0",language:"auto",fixed:"true",vertical:"center",horizontal:"center",left:"200",top:"15",key:"ec7e3b81ba68459b89f89edc42c4789f"}};
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "20",
+    "cityColor": "FFFFFF",
+    "citySize": "20",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "20",
+    "weatherIconSize": "24",
+    "alertIconSize": "20",
+    "padding": "0px 0px 0px 0px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "center",
+    "horizontal": "center",
+    "left": "200",
+    "top": "15",
+    "key": "ec7e3b81ba68459b89f89edc42c4789f"
+  }
+}
