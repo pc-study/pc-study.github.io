27437b405b452abe624ed2e2f8a955c33e48ffc5
pc1107750981@163.com <pc1107750981@163.com>
2022-03-19 13:32:00 +0800

    Site updated: 2022-03-19 13:31:59

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ workbox.core.clientsClaim();
 
 // 注册成功后要立即缓存的资源列表
 // 具体缓存列表在gulpfile.js中配置，见下文
-workbox.precaching.precacheAndRoute([{"revision":"1e22cf2a8149d377efcca1b3593dc38d","url":"./404.html"},{"revision":"59dbff68648fcfd1f02976f30916368c","url":"./index.html"},{"revision":"c2d6628801fd2dc0ea1739901cf5d99b","url":"./js/main.js"},{"revision":"96cb0df52e878a796667210791c90b35","url":"./css/index.css"}],{
+workbox.precaching.precacheAndRoute([{"revision":"79fe265b91c02a0cd7127214d72cbfb1","url":"./404.html"},{"revision":"173283a0ce03358bf133918ccd87afc7","url":"./index.html"},{"revision":"c2d6628801fd2dc0ea1739901cf5d99b","url":"./js/main.js"},{"revision":"96cb0df52e878a796667210791c90b35","url":"./css/index.css"}],{
     directoryIndex: null
 });
 
